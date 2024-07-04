4456e19e6f029d846fbedc8e71c2fbcee66be023
Marcos Vieira <marcosvieira482@gmail.com>
2024-07-04 00:15:19 -0300

    feat: update pages

---
+++ itens.js
@@ -0,0 +1,199 @@
+const itens = [
+    {
+        name: "MODELO PERS.",
+        image: "img/home/personalizada-2.jpg",
+        tag: "home",
+    },
+
+    {
+        name: "MODELO FARDA",
+        image: "img/home/farda-1.jpg",
+        tag: "home",
+    },
+
+    {
+        name: "MODELO PERS.",
+        image: "img/home/personalizada-1.jpg",
+        tag: "home",
+    },
+
+    {
+        name: "MODELO FARDA",
+        image: "img/home/farda-2.jpg",
+        tag: "home",
+    },
+
+    {
+        name: "MODELO JUNINA",
+        image: "img/juninas/junina-3.jpg",
+        tag: "home",
+    },
+
+    {
+        name: "MODELO ABADÁ",
+        image: "img/abada/modelo-abada-6.jpg",
+        tag: "home",
+    },
+
+    {
+        name: "MODELO ABADÁ",
+        image: "img/abada/modelo-abada-7.jpg",
+        tag: "home",
+    },
+
+    {
+        name: "MODELO RELIGIOSA",
+        image: "img/religiosos/religiosa-4.jpg",
+        tag: "home",
+    },
+
+
+
+
+
+    //------------------------------------------------------------------------------------------
+    {
+        name: "Modelo 01",
+        image: "img/abada/modelo-abada-1.jpg",
+        tag: "abada",
+    },
+
+    {
+        name: "Modelo 02",
+        image: "img/abada/modelo-abada-2.jpg",
+        tag: "abada",
+    },
+
+    {
+        name: "Modelo 03",
+        image: "img/abada/modelo-abada-3.jpg",
+        tag: "abada",
+    },
+
+    {
+        name: "Modelo 04",
+        image: "img/abada/modelo-abada-4.jpg",
+        tag: "abada",
+    },
+
+    {
+        name: "Modelo 05",
+        image: "img/abada/modelo-abada-5.jpg",
+        tag: "abada",
+    },
+
+    {
+        name: "Modelo 06",
+        image: "img/abada/modelo-abada-6.jpg",
+        tag: "abada",
+    },
+
+    {
+        name: "Modelo 07",
+        image: "img/abada/modelo-abada-7.jpg",
+        tag: "abada",
+    },
+
+    {
+        name: "Modelo 08",
+        image: "img/abada/modelo-abada-8.jpg",
+        tag: "abada",
+    },
+    //------------------------------------------------------------------------------------------
+    {
+        name: "Modelo 01",
+        image: "img/juninas/junina-1.jpg",
+        tag: "juninas",
+    },
+
+    {
+        name: "Modelo 02",
+        image: "img/juninas/junina-2.jpg",
+        tag: "juninas",
+    },
+
+    {
+        name: "Modelo 03",
+        image: "img/juninas/junina-3.jpg",
+        tag: "juninas",
+    },
+
+    {
+        name: "Modelo 04",
+        image: "img/juninas/junina-4.jpg",
+        tag: "juninas",
+    },
+
+    {
+        name: "Modelo 05",
+        image: "img/juninas/junina-5.jpg",
+        tag: "juninas",
+    },
+
+    {
+        name: "Modelo 06",
+        image: "img/juninas/junina-6.jpg",
+        tag: "juninas",
+    },
+
+    {
+        name: "Modelo 07",
+        image: "img/juninas/junina-7.jpg",
+        tag: "juninas",
+    },
+
+    {
+        name: "Modelo 08",
+        image: "img/juninas/junina-8.jpg",
+        tag: "juninas",
+    },
+    //------------------------------------------------------------------------------------------
+    {
+        name: "Modelo 01",
+        image: "img/religiosos/religiosa-1.jpg",
+        tag: "religiosos",
+    },
+
+    {
+        name: "Modelo 02",
+        image: "img/religiosos/religiosa-2.jpg",
+        tag: "religiosos",
+    },
+
+    {
+        name: "Modelo 03",
+        image: "img/religiosos/religiosa-3.jpg",
+        tag: "religiosos",
+    },
+
+    {
+        name: "Modelo 04",
+        image: "img/religiosos/religiosa-4.jpg",
+        tag: "religiosos",
+    },
+
+    {
+        name: "Modelo 05",
+        image: "img/religiosos/religiosa-5.jpg",
+        tag: "religiosos",
+    },
+
+    {
+        name: "Modelo 06",
+        image: "img/religiosos/religiosa-6.jpg",
+        tag: "religiosos",
+    },
+
+    {
+        name: "Modelo 07",
+        image: "img/religiosos/religiosa-7.jpg",
+        tag: "religiosos",
+    },
+
+    {
+        name: "Modelo 08",
+        image: "img/religiosos/religiosa-8.jpg",
+        tag: "religiosos",
+    },
+
+];
--- script.js
+++ script.js
@@ -8,3 +8,27 @@ function busca() {
         window.location.href = "junino.html";
     }
 }
+
+function addItens(page) {
+    for (var i = 0; i < itens.length; i++) {
+        if (itens[i].tag === page) {
+
+            const div = document.createElement("div");
+            const image = document.createElement("img");
+            const p = document.createElement("p");
+            const btn = document.createElement("button");
+
+            div.classList.add("item");
+            image.src = itens[i].image;
+            p.innerText = itens[i].name;
+            btn.innerText = "VER PRODUTO"
+            btn.classList.add("ver");
+            btn.addEventListener("click", () => {
+                //document.querySelector(".image_modal").style.display = "flex";
+                //document.querySelector(".image_form").innerHTML = `<img src="${itens}" alt="">`;
+            })
+            div.append(image, p, btn);
+            document.getElementById("conteudo").appendChild(div)
+        }
+    }
+}
